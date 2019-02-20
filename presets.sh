3eefec671689c42a52ee696e947e9a9c2f63e56a
LucasDucau <10985059+LucasDucau@users.noreply.github.com>
2019-02-19 23:38:30 -0300

    Delete presets.sh

--- presets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export FLASK_APP=application.py
-export FLASK_ENV=development
-export FLASK_DEBUG=1
-export DATABASE_URL=postgres://riajdstlbetmlj:5e6b8646685966c582745791dda7a2ef7745d3c7d775e48830923ad8bf0ccb32@ec2-23-21-165-188.compute-1.amazonaws.com:5432/d6qn62rv8jqhn3
